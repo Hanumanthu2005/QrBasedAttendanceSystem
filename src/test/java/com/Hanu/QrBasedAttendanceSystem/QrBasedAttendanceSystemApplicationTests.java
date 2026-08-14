a35afaae15d2927c3569ae57ad13abb3474ff2b7
Hanumanthu2005 <hanumanthurao9381@gmail.com>
2026-08-14 13:44:04 +0530

    inti commit created the work environment and connected with the database

---
+++ src/test/java/com/Hanu/QrBasedAttendanceSystem/QrBasedAttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hanu.QrBasedAttendanceSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrBasedAttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
